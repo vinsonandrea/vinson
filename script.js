738c12d755e7be14d8a50595e2413b261398b70f
vinsonandrea <c20245391.vinson@csav.edu.ph>
2026-09-11 09:02:12 +0800

    First Javascript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello, World!");
+
+
+
+
+const myName = "Andrea Ammell vinson";
+let age = 20;
+const number = "638271709";
+const address = "Brgy.1o, Victorias City, Negros Occidental";
+
+
+
+console.log (`Name: ${myName}`);
+console.log (`Age: ${age}`);
+console.log (`Number: ${number}`);
+console.log (`Address: ${address}`);
