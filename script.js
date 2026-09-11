4554aa25be9f15f1ebef1ad74383a50e19889761
vinsonandrea <c20245391.vinson@csav.edu.ph>
2026-09-11 09:20:16 +0800

    variaqbles (let & const)

--- script.js
+++ script.js
@@ -14,3 +14,10 @@ console.log (`Name: ${myName}`);
 console.log (`Age: ${age}`);
 console.log (`Number: ${number}`);
 console.log (`Address: ${address}`);
+
+
+function greet(name) {
+    return `Good morning, ${name}`;
+}
+
+console.log(greet(myName));
