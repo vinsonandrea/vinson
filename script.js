addb9e943e569d4a66954c1cbf0980070256c3d2
vinsonandrea <c20245391.vinson@csav.edu.ph>
2026-09-11 11:51:36 +0800

    addEventListener

--- script.js
+++ script.js
@@ -65,3 +65,23 @@ projectsHeading.style.color = "purple";
 servicesHeading.style.color = "purple";
 
 
+//Event Listeners
+heading.addEventListener("click", function() {
+    heading.style.color = "red";
+});
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener("click", function() {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+});
